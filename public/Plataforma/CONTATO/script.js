0a86aba44ff9c371fd82416771955a77c72d9a2f
sophture <sophia.balboni@sptech.school>
2024-05-23 18:11:19 -0300

    Alterações na tela de contato

--- public/Plataforma/CONTATO/script.js
+++ public/Plataforma/CONTATO/script.js
@@ -2,14 +2,23 @@ function voltarHome() {
     window.location.href = '../SITE/index.html';
 }
 
+const pergunta_1 = 'Com que frequência os sensores precisam ser calibrados?'
+const pergunta_2 = "Os sensores são à prova d'água?"
+const pergunta_3 = 'Quais são os requisitos de manutenção para os sensores?'
+const pergunta_4 = 'Como eu faço para cadastrar um novo funcionário?'
+const pergunta_5 = 'Como cadastrar um novo armazém?'
+const pergunta_6 = 'Como eu sei quais são os últimos do mês?'
+
+const resposta_1 = 'Os sensores serão calibrados a cada três meses para evitar mal funcionamento.'
+const resposta_2 = "Não, nossos sensores não são a prova d'água."
+const resposta_3 = 'É importante garantir que os sensores estejam limpos e livres de empecilhos que possam afetar as medições.'
+const resposta_4 = 'Para cadastrar um novo funcionário basta você acessar o seu perfil e clicar no botão “+” e informar os dados do novo funcionário.'
+const resposta_5 = 'Para cadastrar um novo armazém, basta você fazer a solicitação clicando em “Solicitar um Novo Armazém” na página de perfil.'
+const resposta_6 = 'Basta acessar a aba de histórico e selecionar dentre as opções os registros do mês.'
+
+
 function perguntar() {
     const num_pergunta = select_pergunta.value
-
-    p_pergunta.innerHTML = ''
-    p_resposta.innerHTML = ''
-
-    let pergunta_1 = 'Como funciona a solução?'
-    let resposta_1 = 'O projeto tem como objetivo capturar e monitorar dados de temperatura e umidade, através de sensores que serão instalados em armazéns de grãos de milho.'
 
     // Animação
     let usuario = document.getElementById('pergunta_usuario') 
@@ -25,7 +34,22 @@ function perguntar() {
     if (num_pergunta == '1') {
         p_pergunta.innerHTML = pergunta_1
         p_resposta.innerHTML = resposta_1
-    }
+    } else if (num_pergunta == '2') {
+        p_pergunta.innerHTML = pergunta_2
+        p_resposta.innerHTML = resposta_2
+    } else if (num_pergunta == '3') {
+        p_pergunta.innerHTML = pergunta_3
+        p_resposta.innerHTML = resposta_3
+    } else if (num_pergunta == '4') {
+        p_pergunta.innerHTML = pergunta_4
+        p_resposta.innerHTML = resposta_4
+    } else if (num_pergunta == '5') {
+        p_pergunta.innerHTML = pergunta_5
+        p_resposta.innerHTML = resposta_5
+    } else {
+        p_pergunta.innerHTML = pergunta_6
+        p_resposta.innerHTML = resposta_6
+    } 
 }
 
 function TrocarClasse() {
